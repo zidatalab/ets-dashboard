a84224625da3665f3fa1a9dc59a5e44ed7c64669
Niclas <niclas.wormann@gmail.com>
2024-03-07 13:00:44 +0100

    reimplemented and reworked password change handling

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/services/api.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,111 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private api: ApiService,
+    private router: Router
+  ) { }
+
+  user: any = {}
+  isChangePassword: boolean = false
+  passwordChangeRequest: any = { 'oldpwd': '', 'newpwd': '', 'newpwd_confirm': '' }
+  deleteAccountRequest: any = { 'pwd': '' }
+  error: any = {}
+  passwordVisibilityIcon: string = 'visibility'
+  isPasswordMatchError: boolean = false
+  passwordMatchError: string = 'Die Passwörter stimmen nicht überein.'
+  isPasswordValidateError: boolean = false
+  passwordValidateError: string = 'Das Passwort muss mindestens 8 Zeichen lang sein, eine Zahl enthalten, einen Großbuchstaben und darf keine Sonderzeichen enthalten.'
 
   ngOnInit(): void {
+    this.user = this.getUser()
+  }
+
+  async getUser() {
+    this.user = await this.auth.getUserDetails()
+  }
+
+  openChangePassword() {
+    this.isChangePassword = true
+  }
+
+  closeChangePassword() {
+    this.passwordChangeRequest = { 'oldpwd': '', 'newpwd': '', 'newpwd_confirm': '' }
+    this.isChangePassword = false
+  }
+
+  changePassword() {
+    if (!this.isPasswordMatchError) {
+      this.api.changePassword(this.user.email, this.passwordChangeRequest.newpwd, this.passwordChangeRequest.oldpwd).subscribe(data => {
+        this.isChangePassword = false
+        this.auth.logout()
+        window.location.reload();
+        this.router.navigate(['/'])
+        alert('Passwort wurde geändert. Bitte loggen Sie sich mit ihrem neuen Passwort ein.')
+      }, error => {
+        this.isChangePassword = false
+        this.error = error.error.error
+        alert(this.error)
+      })
+    }
+  }
+
+  checkPasswordMatch() {
+    this.isPasswordMatchError = this.passwordChangeRequest.newpwd !== this.passwordChangeRequest.newpwd_confirm
+  }
+
+  validatePassword() {
+    // Password must be 6-20 characters long
+    // Password must contain at least one uppercase letter
+    // Password must contain at least one number
+    // Password must not contain special characters
+    // Password must not contain whitespace
+
+    if (this.passwordChangeRequest.newpwd.length < 6 || this.passwordChangeRequest.newpwd.length > 20) {
+      this.isPasswordValidateError = !this.isPasswordValidateError;
+    }
+
+    if (!/[A-Z]/.test(this.passwordChangeRequest.newpwd)) {
+      this.isPasswordValidateError = !this.isPasswordValidateError;;
+    }
+
+    if (!/\d/.test(this.passwordChangeRequest.newpwd)) {
+      this.isPasswordValidateError = !this.isPasswordValidateError;;
+    }
+
+    if (/[^A-Za-z0-9]/.test(this.passwordChangeRequest.newpwd)) {
+      this.isPasswordValidateError = !this.isPasswordValidateError;;
+    }
+
+    if (/\s/.test(this.passwordChangeRequest.newpwd)) {
+      this.isPasswordValidateError = !this.isPasswordValidateError;;
+    }
+
+    this.isPasswordValidateError = !this.isPasswordValidateError;;
+  }
+
+  showPassword(id: string) {
+    const passwordField = document.getElementById(id) as HTMLInputElement;
+
+    if (passwordField.type === "password") {
+      this.passwordVisibilityIcon = 'visibility_off'
+      passwordField.type = "text";
+    } else {
+      this.passwordVisibilityIcon = 'visibility'
+      passwordField.type = "password";
+    }
+  }
+
+  deleteAccount() {
+    this.api.deleteUser(this.user.email, this.deleteAccountRequest.pwd).subscribe(data => {
+      this.auth.logout()
+      this.router.navigate(['/'])
+    }, error => {
+      this.error = error.error.error
+      alert(this.error)
+    })
   }
 
 }
+

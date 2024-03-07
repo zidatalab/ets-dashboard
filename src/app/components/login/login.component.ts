fdf3ef0162935c827ecb62ac150da141263e1d6b
Niclas <niclas.wormann@gmail.com>
2024-03-07 15:12:32 +0100

    rebuild fix

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -29,7 +29,7 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
     this.isLoginPending = false
     this.form = this.fb.group({
-      email: ['', Validators.required],
+      username: ['', Validators.required],
       password: ['', Validators.required]
     })
 
